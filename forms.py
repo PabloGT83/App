be70e67f328b6e7d237f76afef9f39b95656ef5f
Manuel Angel Garcia Caballero <manuelcaballero@gmail.com>
2021-04-07 22:31:02 +0200

    Applying css and improving look and feel

--- forms.py
+++ forms.py
@@ -10,5 +10,5 @@ from wtforms import StringField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import DataRequired
 
 class UserForm(FlaskForm):
-  user =  StringField("user", validators=[DataRequired()])
+  user =  StringField(label="", validators=[DataRequired()], description="")
   submit = SubmitField("Try a Twitter user!!")
